bd350bbb19567bcb271ff75a045ba80e651063b3
Laloeyudik <99laluwahyudi@gmail.com>
2024-06-15 01:13:18 +0800

    pembaruan kode

--- app/uploadMasal/domain/uploadMasal.js
+++
@@ -1,8 +0,0 @@
-const xlsx = require("xlsx")
-const fs = require("fs")
-
-const workBook = xlsx.readFile("uploadMasalProduk.xlsx")
-const sheetNames = workBook.SheetNames[0]
-const workSheet = workBook.Sheets[sheetNames]
-
-console.log(workSheet);
